44c0086c23571c9b249981d0596eac16546b58e5
vladyslavJs <vladyslavmelnyk15@gmail.com>
2025-08-05 20:28:40 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import css from './HomePage.module.css';
+
+export default function HomePage() {
+    return (
+        <div className={css.wrapper}>
+            <p className={css.main}>Unisex Outfits.</p>
+            <p className={css.main}>Trendy Styles.</p>
+            <p className={css.add}>Soft, Cosy Fabrication And Modern Styling.</p>
+        </div>
+    );
+}
